e68898780b0577651772a4a45d917770b6557a99
aditya8281 <b24ee1062@iitj.ac.in>
2025-05-10 20:22:23 +0530

    Added Normal Python code file
    This file contains code which i wrote for practicing python...

---
+++ Python/Practice codes/Hello.py
@@ -0,0 +1,33 @@
+# n=input("Enter num : ")
+
+# m = len(n)
+# num = int(n)
+# l=[]
+
+# pal = True
+
+
+# for i in range(0,m):
+#     l.append(num%10)
+#     num = (num-num%10)/10
+
+# for i in range(0,m):
+#     if( l[i] == l[m-i-1]):
+#         pal = True
+#     else: pal=False
+
+# if(pal): print("is Pal")
+# else : print("is not")
+
+#---------------------------------------------------------------------
+
+# d = { "name" : "Aditya" , "Roll":"B24EE1062"}
+# d["Year"] = 1
+# print(d)
+
+#---------------------------------------------------------------------
+
+t = (1,2,"Aditya")
+
+m = t.count("Aditya")
+print(m)
